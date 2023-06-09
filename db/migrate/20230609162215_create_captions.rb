eebf42e193edbe8d793e7ca2a987dccb32ab3a59
davamo <8826599-davamo@users.noreply.gitlab.com>
2023-06-09 12:34:03 -0400

    feat: Caption photo_id:integer text:string and Add  AddCommentIdToCaptions comment_id:integer

---
+++ db/migrate/20230609162215_create_captions.rb
@@ -0,0 +1,10 @@
+class CreateCaptions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :captions do |t|
+      t.integer :photo_id
+      t.string :text
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_09_161850) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_09_163249) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "captions", force: :cascade do |t|
+    t.integer "photo_id"
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "comment_id"
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
